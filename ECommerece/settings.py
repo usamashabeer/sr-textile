3347e959d2aba2220781a83a14fe3dcd039cf232
hp <usamashabeer1@gmail.com>
2020-11-02 23:51:59 +0500

    1

--- ECommerece/settings.py
+++ ECommerece/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '-hhx0r1i_$*d32j7j1^t$vqc&&r4addd4&0(^+xetlm*m%uuco'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['ECommerece.herokuapp.com', 'localhost', '127.0.0.1']
 
